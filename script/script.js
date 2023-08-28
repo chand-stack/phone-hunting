600c716d5206bacff18fc1aa4bb3d5976e1ceaa7
chand-stack <chand.stack@gmail.com>
2023-08-28 08:47:38 +0600

    js practice

--- script/script.js
+++ script/script.js
@@ -25,7 +25,7 @@ const displayPhones = (phones, isShowAll) => {
     }
 
 phones.forEach(phone => {
-    console.log(phone)
+    // console.log(phone)
 
     const phoneDiv = document.createElement('div')
     phoneDiv.classList = `card p-4 bg-base-100 shadow-xl`;
@@ -34,8 +34,8 @@ phones.forEach(phone => {
     <div class="card-body">
       <h2 class="text-center font-bold text-lg">${phone.phone_name}</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+      <div class="card-actions justify-center">
+        <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
       </div>
     </div>
     `;
@@ -43,6 +43,44 @@ phones.forEach(phone => {
     
 });
 toggleLoadingSpinner(false)
+}
+
+const handleShowDetail = async (id) => {
+  // console.log('show detail',id)
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  const data = await res.json();
+  const phone = data.data
+  showPhoneDetails(phone)
+}
+
+const showPhoneDetails = (phone) => {
+console.log(phone)
+  const phoneContainer = document.getElementById('show-modal-container');
+
+  const detaiModal = document.createElement('div')
+  detaiModal.classList.add('class="space-y-1')
+  detaiModal.innerHTML =`
+  <img class="mx-auto" src="${phone.image
+  }" alt="">
+  <h1 class="font-bold text-2xl">${phone.name}</h1>
+  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+  <p><span class="font-bold">Storage: </span>${phone.mainFeatures.storage
+  }</p>
+  <p><span class="font-bold">Display Size: </span>${phone.mainFeatures.displaySize
+  }</p>
+  <p><span class="font-bold">Chipset: </span>${phone.mainFeatures.chipSet
+  }</p>
+  <p><span class="font-bold">Memory: </span>${phone.mainFeatures.memory
+  }</p>
+  <p><span class="font-bold">Slug: </span>${phone.slug
+  }</p>
+  <p><span class="font-bold">Release Date: </span>${phone.releaseDate
+  }</p>
+  <p><span class="font-bold">Brand: </span>${phone.brand
+  }</p>
+  `
+phoneContainer.appendChild(detaiModal)
+  show_details_modal.showModal()
 }
 
 const handleSearch = (isShowAll) => {
